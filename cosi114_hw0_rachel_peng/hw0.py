cae18dce34c6d7850464cf8e9d060b01d1501763
Rachel Peng <xpeng@brandeis.edu>
2020-09-10 05:10:26 +0800

    added comments to TA

--- cosi114_hw0_rachel_peng/hw0.py
+++ cosi114_hw0_rachel_peng/hw0.py
@@ -3,6 +3,10 @@ import regex
 _RE_PUNC = regex.compile(r"^\p{p}$")
 _EM_DASH = "-"
 
+#To the grading TA:
+#I apoplogize for this low-performing code. I had a lot going on this week and I need to catch up with the course work.
+#I have issue with importing regex, but I didn't have time to go to any office hour for debugging.
+
 #1. Generating sentences
 #how to declare path variable?
 
